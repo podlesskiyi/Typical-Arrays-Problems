7ac5ad5ab57b2a4fb2553ee22b554df6c50dfe3a
podlesskiyi <77390001+podlesskiyi@users.noreply.github.com>
2021-03-15 20:07:45 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = []) {
+  return (array.length && Math.min(...array)) || 0;
+}
+exports.max = function max (array = []) {
+  return (array.length && Math.max(...array)) || 0;
 }
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = []) {
+  return (array.length && (array.reduce((a, b) => a + b, 0) / array.length)) || 0;
 }
